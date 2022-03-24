205921aea5f9f040d70e702b15c4c5935bdcac1b
Luis Carrera <luiscarreramv@gmail.com>
2022-03-24 02:11:03 -0600

    Se finalizo el contador

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import './App.css';
+import Button from './components/Button';
+import Counter from './components/Counter';
+import luisCarreraLogo from './images/luiswhite.png';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+
+  const [numClicks, setNumClicks] = useState(0);
+
+  const doClick = () => {
+    setNumClicks(numClicks + 1);
+  };
+
+  const resetCounter = () => {
+    setNumClicks(0);
+  };
+
   return (
     <div className="App">
-      
+      <div className='luiscarrera-logo-container'>
+        <img
+          className='luiscarrera-logo'
+          src={luisCarreraLogo}
+          alt='Logo Luis Carrera' />
+      </div>
+      <div className="main-container">
+        <Counter 
+          clicksNumber={numClicks} />
+        <Button 
+          text='Click'
+          isClickButton={true}
+          doClick={doClick} />
+        <Button 
+          text='Reset'
+          isClickButton={false}
+          doClick={resetCounter} />
+      </div>
     </div>
   );
 }
